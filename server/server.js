6600a25e27349ef2bc8f932b63eb55314d9e7bc0
Kim Ho Jung <kimhojung@Kimui-MacBookPro.local>
2019-09-29 15:06:04 +0900

    upload

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.start = function() {
   });
 };
 
-
+app.getModel('members')
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
