474ae7935cd385ede4d855545c542bea484ce277
Kim Ho Jung <kimhojung@Kimui-MacBookPro.local>
2019-09-29 15:45:03 +0900

    upload

--- common/models/member.js
+++ common/models/member.js
@@ -4,10 +4,11 @@ module.exports = function(Member) {
 
 
     Member.signup = function(msg, cb){
-  
+        cb(null, 'signup' + msg)
     } 
 
   Member.remoteMethod('signup',{
-  
+    accepts : {arg:'msg', type: 'string'},
+    returns : {arg: 'greeting', type: 'strging'}
   });  
 };
