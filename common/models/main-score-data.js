85cc74523cc3b6f064f989dc21248c85d67bea34
Kim Ho Jung <kimhojung@HoJung-Mac.local>
2019-12-07 16:44:54 +0900

    upload

--- common/models/main-score-data.js
+++ common/models/main-score-data.js
@@ -2,6 +2,22 @@
 
 module.exports = function(Mainscoredata) {
 
+    Mainscoredata.getMyScore = function(name, date, cb){
+        Mainscoredata.getDataSource().connector.connect(function(err,db){
+            var collection = db.collection('mainScoreData');
+            collection.find({'username':name, 'date': date}).toArray(function(err,res){
+                cb(null,res);
+            });
+        });
+    };
 
-    
+    Mainscoredatab.remoteMethod('getMyScore',{
+        accepts : [
+            { arg: 'getName', type: 'string', require: true },
+            { arg: 'getDate', type: 'string', require: true }
+        ],
+        returns : { arg: 'result', type: 'object'},
+        http : { path : '/getMyScore',verb: 'get'}
+    });
+
 };
