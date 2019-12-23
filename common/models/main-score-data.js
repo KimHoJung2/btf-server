6412fb3ef604fe2fcc2e3933fd2d40912b0e93da
Kim Ho Jung <kimhojung@HoJung-Mac.local>
2019-12-23 17:23:31 +0900

    upload

--- common/models/main-score-data.js
+++ common/models/main-score-data.js
@@ -2,6 +2,8 @@
 
 module.exports = function(Mainscoredata) {
 
+    var ObjectId = require('mongodb').ObjectId;
+
     Mainscoredata.getMyScore = function(name, date, cb){
         Mainscoredata.getDataSource().connector.connect(function(err,db){
             var collection = db.collection('mainScoreData');
@@ -14,7 +16,7 @@ module.exports = function(Mainscoredata) {
     Mainscoredata.getScoreAll = function(id, cb){
         Mainscoredata.getDataSource().connector.connect(function(err,db){
             var collection = db.collection('mainScoreData');
-            collection.find({'boardId': id}).toArray(function(err,res){
+            collection.find({'boardId': ObjectId(id)}).toArray(function(err,res){
                 cb(null,res);
             });
         });
