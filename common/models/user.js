9ec7c5c523c55dd1c75c1c1d8f361327069b6eb7
Kim Ho Jung <kimhojung@HoJung-Mac.local>
2019-12-24 17:43:52 +0900

    upload

--- common/models/user.js
+++ common/models/user.js
@@ -1,5 +1,22 @@
 'use strict';
 
 module.exports = (User) => {
-  
+    var ObjectId = require('mongodb').ObjectId;
+
+    User.getUser = function(id, cb){
+        User.getDataSource().connector.connect(function(err,db){
+            var collection = db.collection('user');
+            collection.find({'_id':ObjectId(id)}).toArray(function(err,res){
+                cb(null,res);
+            });
+        });
+    };
+
+    User.remoteMethod('getUser',{
+        accepts : [
+            { arg: 'getUser', type: 'string', require: true }
+        ],
+        returns : { arg: 'result', type: 'object'},
+        http : { path : '/getUser',verb: 'get'}
+    });
 };
