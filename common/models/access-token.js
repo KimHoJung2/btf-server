ea6a409a29431a51437ef75d6478b98bd658feed
Kim Ho Jung <kimhojung@HoJung-Mac.local>
2019-12-24 17:03:50 +0900

    upload

--- common/models/access-token.js
+++ common/models/access-token.js
@@ -1,4 +1,22 @@
 'use strict';
 
-module.exports = (/* AccessToken */) => {
+module.exports = (AccessToken) => {
+
+    AccessToken.getUser = function(id, cb){
+        AccessToken.getDataSource().connector.connect(function(err,db){
+            var collection = db.collection('mainScoreData');
+            collection.find({'_id':id}).toArray(function(err,res){
+                cb(null,res);
+            });
+        });
+    };
+
+    Mainscoredata.remoteMethod('getUser',{
+        accepts : [
+            { arg: 'getUser', type: 'string', require: true }
+        ],
+        returns : { arg: 'result', type: 'object'},
+        http : { path : '/getUser',verb: 'get'}
+    });
+
 };
