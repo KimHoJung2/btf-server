0b9aaa318f0d4f82a82a3b47e68785d0bc996a15
Kim Ho Jung <kimhojung@HoJung-Mac.local>
2019-12-28 16:06:26 +0900

    upload

--- common/models/attend-user.js
+++ common/models/attend-user.js
@@ -3,12 +3,21 @@
 module.exports = function(Attenduser) {
 
     var ObjectId = require('mongodb').ObjectId;
-    
+
     Attenduser.getUser = function(name, id, cb){
         Attenduser.getDataSource().connector.connect(function(err,db){
             var collection = db.collection('attendUser');
             collection.find({'username':name},{'boardId': ObjectId(id)}).toArray(function(err,res){
                 cb(null,res);
+            });
+        });
+    };
+    
+    Attenduser.deleteUser = function(name, id, cb){
+        Attenduser.getDataSource().connector.connect(function(err,db){
+            var collection = db.collection('attendUser');
+            collection.deleteOne({'username':name},{'boardId': ObjectId(id)}).then(function(res){
+                return res
             });
         });
     };
@@ -20,5 +29,22 @@ module.exports = function(Attenduser) {
         ],
         returns : { arg: 'result', type: 'object'},
         http : { path : '/getUser',verb: 'get'}
+    });
+
+    Attenduser.remoteMethod('deleteUser',{
+        accepts : [
+            { arg: 'getName', type: 'string', require: true },
+            { arg: 'getBoardId', type: 'string', require: true }
+        ],
+        returns : { arg: 'result', type: 'object'},
+        http : { path : '/getUser',verb: 'get'}
+    });
+    
+    Attenduser.remoteMethod('deleteUser',{
+        accepts : [
+            { arg: 'deleteName', type: 'string', require: true },
+            { arg: 'deleteBoardId', type: 'string', require: true },
+        ],
+        returns: { arg: 'Content-Type', type: 'string', http: { target: 'header' }}
     });
 };
