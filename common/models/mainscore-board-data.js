add4a2a01c8efabc04ce237fb749e689596da438
Kim Ho Jung <kimhojung@HoJung-Mac.local>
2019-12-29 15:20:47 +0900

    upload

--- common/models/mainscore-board-data.js
+++ common/models/mainscore-board-data.js
@@ -2,10 +2,23 @@
 
 module.exports = function(Mainscoreboarddata) {
 
+    var ObjectId = require('mongodb').ObjectId;
 
-    
+    Mainscoreboarddata.getBoard = function(id, cb){
+        Mainscoreboarddata.getDataSource().connector.connect(function(err,db){
+            var collection = db.collection('mainscoreBoardData');
+            collection.find({'boardId': ObjectId(id)}).toArray(function(err,res){
+                cb(null,res);
+            });
+        });
+    };
 
-
-
+    Mainscoreboarddata.remoteMethod('getBoard',{
+        accepts : [
+            { arg: 'getId', type: 'string', require: true }
+        ],
+        returns : { arg: 'result', type: 'object'},
+        http : { path : '/getBoard',verb: 'get'}
+    });
 
 };
