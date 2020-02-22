569fccf27be75ecbb3e8de8e5bcbc1c65956b985
Kim Ho Jung <ghwjd7376@naver.com>
2020-02-22 15:38:33 +0900

    payment findOne test

--- common/models/user.js
+++ common/models/user.js
@@ -3,17 +3,26 @@
 module.exports = (User) => {
     var ObjectId = require('mongodb').ObjectId;
 
-    User.getUser = function(id, cb){
+    User.getUser = function(id, name, cb){
         User.getDataSource().connector.connect(function(err,db){
             var collection = db.collection('user');
-            collection.find({'_id':ObjectId(id)}).toArray(function(err,res){
-                cb(null,res);
-            });
+            if(name === undefined){
+                collection.find({'_id':ObjectId(id)}).toArray(function(err,res){
+                    cb(null,res);
+                });
+            }else{
+                collection.find({'username':name}).toArray(function(err,res){
+                    cb(null,res);
+                });
+            }
+            
+            
         });
     };
     User.remoteMethod('getUser',{
         accepts : [
-            { arg: 'getUser', type: 'string', require: true }
+            { arg: 'getUser', type: 'string', require: true },
+            { arg: 'getName', type: 'string', require: true }
         ],
         returns : { arg: 'result', type: 'object'},
         http : { path : '/getUser',verb: 'get'}
